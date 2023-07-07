5d7be1b8abc2d4683060c2689a95e90219c1c1e2
Leslie <Kooliesk@gmail.com>
2023-07-07 16:43:48 +0200

    add categories controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+    before_action :current_user
+  
+    private
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
+    def after_sign_in_path_for(resource)
+      categories_path
+    end
+  end
+  
